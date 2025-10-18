9ff783a92a9f822c88290cac471bbf29d5316134
Gerlyn Tan <gerlyntan07@gmail.com>
2025-10-18 14:46:31 +0800

    Comment out VITE_API_URL in api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Use environment variable if available (e.g. for production)
-const BASE_URL = import.meta.env.VITE_API_URL || 
+const BASE_URL = //import.meta.env.VITE_API_URL || 
 "http://localhost:8000";
 
 // Create an Axios instance
